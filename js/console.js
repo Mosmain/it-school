dd236a0e1645e6eec6b09c1535ff64d7b9623919
SergeyMoskvichev <mosmain@yandex.ru>
2020-12-01 23:25:25 +0400

    first

---
+++ js/console.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#typed',{
+  strings: [
+    '>install MS.exe^1000\n`>Waiting for install...`^1900\n`>Welcome to the school!`^800\n`>Les’t start learning!`\n`>`'
+  ],
+  backSpeed: 40,
+  cursorChar: '_',
+  typeSpeed: 50
+});
