95440a729a36853782e834c89fbb522d4f9d7164
SergeyMoskvichev <mosmain@yandex.ru>
2020-12-17 16:57:37 +0400

    pdo

--- dbсonnection.php
+++ dbсonnection.php
@@ -1,14 +1,16 @@
 <?php
+$host = '127.0.0.1';
+$db   = 'it_school';
+$user = 'root';
+$pass = '';
+$charset = 'utf8';
 
-$options = array(
-  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-);
-$pdo = new PDO(
-  'mysql:dbname=it_school;host=localhost',
-  'root',
-  '',
-  array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")
-);
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $opt);
 
 ?>
